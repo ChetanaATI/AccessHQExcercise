ca2920d5de0bb1625314a07338342e8b17a43494
ChetanaHKumar <chetana.skiva@gmail.com>
2018-11-01 08:23:47 +1100

    Add files via upload

---
+++ Exercise/src/main/java/AccessHQ/Exercise/App.java
@@ -0,0 +1,13 @@
+package AccessHQ.Exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
